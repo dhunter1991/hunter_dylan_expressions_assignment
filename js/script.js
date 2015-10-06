45ab807084b34a12d499b4fe2c998ca17c754cdf
dhunter1991 <hunterdylan882@gmail.com>
2015-10-05 20:33:29 -0400

    commit 5

--- js/script.js
+++ js/script.js
@@ -1,46 +1,39 @@
-
 // Dylan Hunter
 // 10/4/2015
-// Scalable Data Infrastructures 01
+//Scalable Data Infrastructures 01
 // Expressions assignment
 
+// calculation for gross profit margin percentage
 
 
-// calculation for gross profit margin percentage //
-
-// Variables
 
 var costOfGoods = prompt("We are going to calculate your gross profit margin. \nenter the total cost of goods");
 var payRoll = prompt("enter in the total cost for payroll");
 var revenue = prompt("enter the total amount of revenue from sales");
+var profit = Number(revenue) - Number(costOfGoods) + Number(payRoll);
+
 // calculating profit
 
 var profit = Number(revenue) - (Number(costOfGoods) + Number(payRoll));
+
 
 var profitConstruction = [costOfGoods, revenue, profit, division, payRoll];
 
 console.log(profit);
 
+// calculating ratio of cost to revenue
+
 var division = profitConstruction [2]  / profitConstruction [1] + profitConstruction [4];
+
 
 var profitConstruction = [costOfGoods, revenue, profit, division, payRoll];
 
 console.log(division);
+
 // calculating profit margin percentage
 
 var profitMargin = profitConstruction [3]  * 100;
 
 console.log(profitMargin);
 
-alert("your companies profit margin is " + profitMargin + " percent");
-
-
-
-
-
-
-
-
-
-
-
+alert("your companies profit margin percentage is " + profitMargin);
